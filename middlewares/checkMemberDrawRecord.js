0504a0f4d61810f59d1bc58cc5c76622bd8837ac
iVan <ivan@iVandeMacBook-Pro.local>
2020-03-18 23:22:41 +0800

    update checkMemberDrawRecord 抽奖次数限制

--- middlewares/checkMemberDrawRecord.js
+++ middlewares/checkMemberDrawRecord.js
@@ -1,7 +1,32 @@
 // 需求：每个会员每天抽奖次数不能超过限制
 
+const prizeRecordModel = require('../models/prizeRecord.model');
+
 async function checkMemberDrawRecord (req, res, next) {
 	try {
+		const records = await prizeRecordModel.find({memberId});
+		if (records && records.length > req.activityInfo.drawLimitTotal) {
+			return res.send({
+				msg:'活动抽奖次数已全部用完',
+				err: true,
+			});
+		}
+		// 每天抽奖次数限制
+		if (req.activityInfo.drawLimitDay) {
+		  const today = moment(new Date()).format('YYYYMMDD');
+		  const todays = records.filter(item => {
+		    const date = moment(item.created_at).format('YYYYMMDD');
+		    return date === today;
+		  });
+		  req.todayDrawRest = req.activityInfo.drawLimitDay - todays.length - 1;
+		}
+		if (!req.todayDrawRest) {
+			return res.send({
+				msg:'今日抽奖次数没有了，明天再来',
+				err: true,
+			});
+		}
+		req.drawRecords = records;
 		next();
 	}
 	catch(e) {
--- routes/drawPrize.route.js
+++ routes/drawPrize.route.js
@@ -7,20 +7,21 @@ const checkMemberRestPoint = require('../middlewares/checkMemberRestPoint');
 const drawPrize = require('../middlewares/drawPrize');
 const updateMemberDrawRecord = require('../middlewares/updateMemberDrawRecord');
 const checkActivityDate = require('../middlewares/checkActivityDate');
-
+const checkMemberDrawRecord = require('../middlewares/checkMemberDrawRecord');
 const routes = new Router();
 
 routes
 	.route('/luckyDraw')
 	.get((req,res,next) => res.send('good luck'))
 	.post(
-		checkMemberAuth,
-		checkActivityDate,
-		checkFreeCode,
-		checkMemberRestPoint,
-		checkRestPrizePool,
-		drawPrize,
-		updateMemberDrawRecord);
+		checkMemberAuth,// 先检查参与抽奖用户权限
+		checkActivityDate, // 检查抽奖活动有效期
+		checkFreeCode, // 检查参与用户渠道来源，是否需要消耗积分参与
+		checkMemberDrawRecord, //检查用户抽奖记录，是否符合活动限制
+		checkMemberRestPoint, // 检查用户积分是否足够，如足够，先扣积分，再参与
+		checkRestPrizePool, // 检查奖池是否有剩余奖品，无奖品，则返回谢谢参与
+		drawPrize, // 抽奖，产生一个随机数号码，对应奖池奖品编号，若有则中奖，若无，则表示未中奖
+		updateMemberDrawRecord); // 最后更新抽奖记录
 
 routes
 	.route('/updatePrizePool')
